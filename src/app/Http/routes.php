60d1332852c355afda139825b5b085bdba82f14c
Kento Takano <kento2github@gmail.com>
2020-02-23 20:37:54 +0900

    Added controller and route and blade template

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+// Route::get('/post/{id}', 'PostsController@index');
+Route::resource('posts', 'PostsController');
+Route::get('/contact', 'PostsController@contact');
+Route::get('post/{id}/{name}/{password}', 'PostsController@show_post');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,31 +16,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/about', function () {
-    return "hi about page";
-});
+// Route::get('/about', function () {
+//     return "hi about page";
+// });
 
-Route::get('/contact', function () {
-    return "hi contact page";
-});
+// Route::get('/contact', function () {
+//     return "hi contact page";
+// });
 
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "hi post page id is $id and name is $name";
-});
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return "hi post page id is $id and name is $name";
+// });
 
-// ルートに対してネームスペースを使用する例
-// 'as'=>'naming' でできる
-Route::get('/admin/posts/example', array('as'=>'admin.home', function() {
-    $url = route('admin.home');
+// // ルートに対してネームスペースを使用する例
+// // 'as'=>'naming' でできる
+// Route::get('/admin/posts/example', array('as'=>'admin.home', function() {
+//     $url = route('admin.home');
 
-    return "this url is $url";
-}));
+//     return "this url is $url";
+// }));
 
 
-Route::group(['middleware' => ['web']], function () {
+// Route::group(['middleware' => ['web']], function () {
 
-});
+// });
