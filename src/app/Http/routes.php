e7e37e298a3686bd6b36ae021220d1824fb8f348
Kento Takano <kento2github@gmail.com>
2020-02-25 07:53:34 +0900

    Eloquent ORM learn

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -1,9 +1,38 @@
 <?php
 
+// use Illuminate\Support\Facades\DB;
+use App\Post;
+
+/**
+ * DATABASE Raw SQL Queries
+ */
+// Route::get('/insert', function() {
+//     DB::insert('insert into posts(title, content) values(?, ?)',
+//         ['PHP with Laravel', 'Laravel is the best thing that has happened to PHP']);
+//             DB::insert('insert into posts(title, content) values(?, ?)',
+//         ['PHP with CakePHP', 'CakePHP is the best thing that has happened to PHP']);
+// });
+
+// Route::get('/read', function () {
+//     $results = DB::select('select * from posts where id=?', [1]);
+
+//     return var_dump($results);
+// });
+
+// Route::get('/update', function() {
+//     $updated = DB::update('update posts set title = "Update title" where id = ?', [1]);
+//     return $updated;
+// });
+
+// Route::get('/delete', function() {
+//     $deleted = DB::delete('delete from posts where id = ?', [1]);
+//     return $deleted;
+// });
+
 // Route::get('/post/{id}', 'PostsController@index');
-Route::resource('posts', 'PostsController');
-Route::get('/contact', 'PostsController@contact');
-Route::get('post/{id}/{name}/{password}', 'PostsController@show_post');
+// Route::resource('posts', 'PostsController');
+// Route::get('/contact', 'PostsController@contact');
+// Route::get('post/{id}/{name}/{password}', 'PostsController@show_post');
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +45,9 @@ Route::get('post/{id}/{name}/{password}', 'PostsController@show_post');
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/about', function () {
 //     return "hi about page";
@@ -44,3 +73,52 @@ Route::get('/', function () {
 // Route::group(['middleware' => ['web']], function () {
 
 // });
+
+////////////////////////////////////
+/**
+ * ELOQUENT
+ */
+
+// Route::get('/find', function() {
+//     $posts = Post::all();
+
+//     foreach($posts as $post) {
+//         return $post->title;
+//     }
+// });
+
+// Route::get('/find', function() {
+//     $post = Post::find(1);
+
+//     return $post->title;
+// });
+
+// Route::get('/findwhere', function() {
+//     $posts = Post::where('id', 3)->orderBy('id', 'desc')->take(1)->get();
+//     return $posts;
+// });
+
+// Route::get('/findmore', function() {
+//     // $posts = Post::findOrFail(2);
+
+//     // return $posts;
+
+//     $posts = Post::where('users_count', '<', 50)->firstOrFail();
+// });
+
+// Route::get('/basicinsert', function() {
+//     $post = new Post;
+
+//     $post->title = "New Eloquent title insert";
+//     $post->content = 'Wow Eloquent is really';
+//     $post->save();
+// });
+
+Route::get('/basicupdate', function() {
+    $post = Post::find(2);
+
+    $post->title = "New Eloquent title insert 2";
+    $post->content = 'Wow Eloquent is really 2';
+    $post->save();
+});
+
